b50d98eca647a88a100c38c4afa349f862ab4be8
Sai Ruthvik <113750504+hawkh@users.noreply.github.com>
2025-07-31 01:03:58 +0530

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,7 @@ import { Footer } from "@/components/footer";
 import { Header } from "@/components/header";
 import { Hero } from "@/components/hero";
 import { Services } from "@/components/services";
+import { Testimonials }from "@/components/testimonials";
 
 export default function Home() {
   return (
