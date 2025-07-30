b0e09aa0eec72d8de0f3e2d34b005b82586458b6
Sai Ruthvik <113750504+hawkh@users.noreply.github.com>
2025-07-31 01:08:29 +0530

    Update hero.tsx

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -30,7 +30,7 @@ export function Hero() {
             alt="Hero"
             className="mx-auto aspect-video overflow-hidden rounded-xl object-cover sm:w-full lg:order-last lg:aspect-square"
             height="550"
-            src="https://images.unsplash.com/photo-1629909613654-28e377c37b09?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=600&q=80"
+            src="/vdc.jpg"
             sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
             width="550"
           />
