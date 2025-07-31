e63096689e6523d00ae5a3c21608047faf3808be
Sai Ruthvik <113750504+hawkh@users.noreply.github.com>
2025-07-31 15:06:43 +0530

    Update booking-form.tsx

--- src/components/booking-form.tsx
+++ src/components/booking-form.tsx
@@ -476,7 +476,7 @@ Thank you!
           </DialogHeader>
           <div className="flex justify-center p-4">
             <Image
-              src="https://images.unsplash.com/photo-1606166187734-a4cb74079037?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=250&q=80"
+              src="/upi&auto=format&fit=crop&w=250&q=80"
               alt="UPI QR Code"
               width={250}
               height={250}
